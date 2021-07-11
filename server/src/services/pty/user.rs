464656205e6b9cc1725f446e2f914674fa4610dd
Tommi Virtanen <tv@eagain.net>
2021-07-11 15:07:37 -0600

    When PTY closes, shutdown incoming IPC to get input reader to exit

--- server/src/services/pty/user.rs
+++ server/src/services/pty/user.rs
@@ -18,6 +18,9 @@ pub(super) enum ServeUserError {
     #[error("socket send error: {0}")]
     Send(#[source] ipc::SendError),
 
+    #[error("error shutting down IPC socket: {0}")]
+    SocketShutdown(#[from] ipc::ShutdownError),
+
     #[error("PTY I/O error: {0}")]
     PtyIo(#[source] std::io::Error),
 }
@@ -49,21 +52,41 @@ pub(super) fn serve_user(
     });
 
     let output: std::thread::JoinHandle<Result<(), ServeUserError>> = std::thread::spawn({
+        let conn = conn.clone();
         move || loop {
             let mut buf = vec![0; 1024];
-            let n = (&*pty).read(&mut buf).map_err(ServeUserError::PtyIo)?;
+            let n = {
+                let result = (&*pty).read(&mut buf);
+                // Map EIO to a regular return.
+                // All PTY interactions end at EIO, there's no EOF.
+                if let Err(error) = &result {
+                    if let Some(errno) = error.raw_os_error() {
+                        if errno == libc::EIO {
+                            return Ok(());
+                        }
+                    }
+                }
+                result.map_err(ServeUserError::PtyIo)?
+            };
             buf.truncate(n);
             let message = p::Output::SessionOutput(buf);
             conn.send_with_fds(&message).map_err(ServeUserError::Send)?;
         }
     });
 
-    // TODO when one fails, cancel the other
-    match input.join() {
-        Err(panicked) => std::panic::resume_unwind(panicked),
-        Ok(result) => result?,
-    };
     match output.join() {
+        Err(panicked) => std::panic::resume_unwind(panicked),
+        Ok(result) => {
+            println!("pty closed: {:?}", result);
+            // Shutdown the IPC socket so the reading thread will exit.
+            //
+            // This will cause sending IPC clients to see EPIPE, but they'll just have to handle that.
+            // The PTY is gone, there's nothing this service can do for them anymore.
+            let how = std::net::Shutdown::Read;
+            conn.shutdown(how).map_err(ServeUserError::SocketShutdown)?;
+        }
+    };
+    match input.join() {
         Err(panicked) => std::panic::resume_unwind(panicked),
         Ok(result) => result?,
     };
